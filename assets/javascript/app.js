c6f2c321afef51ba2cfe6d4f19367ab7f8831bb6
Michael Kim <MichaelKim0717@gmail.com>
2018-06-15 12:36:00 -0400

    Basic layout created. Implemented category selection, test questions, highlight correct/incorrect answers accordingly

---
+++ assets/javascript/app.js
@@ -0,0 +1,181 @@
+/* Arrays for Questions */
+var geoQ = [{question: "Which African country shares a border with Chad?", choices: ["Nigeria", "Egypt", "Mali", "Ethiopia"], cAnswer: "Nigeria"},
+            {question: "What is the name of the islands south of Argentina?", choices: ["Hispaniola", "The Falkland Islands", "The Galapagos Islands", "Labrador"], cAnswer: "The Falkland Islands"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"}];
+
+var sciQ = [{question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"}];
+            
+var histQ = [{question: "What ancient civilization built the Parthenon", choices: ["Romans", "Mesopotamians", "Greeks", "Egyptians"], cAnswer: "Greeks"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"}];
+
+var etQ = [{question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"}];
+
+var spoQ = [{question: "At Super Bowl LI, who flipped the coin at the coin toss?", choices: ["George W Bush", "George H W Bush", "Rob Gronkowski", "Barack Obama"], cAnswer: "George H W Bush"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"}];
+
+var artQ = [{question: "What theatre was Hamilton first shown in?", choices: ["Private Bank Theatre", "Richard Rogers Theatre", "Public Theatre", "Broadway Theatre"], cAnswer: "Public Theatre"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"},
+            {question: "test", choices: ["a", "b", "c", "d"], cAnswer: "b"}];
+
+$(document).ready(function(){
+    
+    /* When the 'Entertainment' button is clicked,
+     * Show random entertainment questions.
+     */
+    $(".ent-btn").on("click", function(){
+        $(".container").empty();
+        $(".container").html(
+            "<h3> " + etQ[0].question + " </h3>" +
+            "<div class='btn-div'>" +
+            "<button class='btn btn-primary' id='choice1'> " + etQ[0].choices[0] + "</button>" +
+            "<button class='btn btn-primary' id='choice2'> " + etQ[0].choices[1] + "</button>" +
+            "<button class='btn btn-primary' id='choice3'> " + etQ[0].choices[2] + "</button>" +
+            "<button class='btn btn-primary' id='choice4'> " + etQ[0].choices[3] + "</button>" +
+            "</div>"
+        );
+    });
+
+    $(".container").on("click", "#choice1", function(){
+        if(etQ[0].choices[0] == etQ[0].cAnswer){
+            $(this).css("background-color", "green");
+        }
+        else {
+            $(this).css("background-color", "red");
+        }
+    })
+
+    $(".container").on("click", "#choice2", function(){
+        if(etQ[0].choices[1] == etQ[0].cAnswer){
+            $(this).css("background-color", "green");
+        }
+        else {
+            $(this).css("background-color", "red");
+        }
+    })
+
+    $(".container").on("click", "#choice3", function(){
+        if(etQ[0].choices[2] == etQ[0].cAnswer){
+            $(this).css("background-color", "red");
+        }
+        else {
+            $(this).css("background-color", "red");
+        }
+    })
+
+    $(".container").on("click", "#choice4", function(){
+        if(etQ[0].choices[3] == etQ[0].cAnswer){
+            $(this).css("background-color", "green");
+        }
+        else {
+            $(this).css("background-color", "red");
+        }
+    })
+});
